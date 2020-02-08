c8c2fb1bdab82f6e8cd731cfffe4f10bce4cf18c
iwannatto <iwannatto@gmail.com>
2020-02-08 16:56:12 +0900

    Separate files to avoid using preprocessor DREG identifier

--- Makefile
+++ Makefile
@@ -4,19 +4,18 @@ run: dry2 dry2nr dry2o
 	./dry2 ${1-50000} 2>/dev/null
 	./dry2nr ${1-50000} 2>/dev/null
 	./dry2o ${1-50000} 2>/dev/null
-	rm -f dry1.o
 
-dry2: dry.c
-	${CC}    -c ${CFLAGS} dry.c -o dry1.o || exit 1
-	${CC} -DPASS2 ${CFLAGS} dry.c dry1.o ${LFLAGS} -o dry2 || exit 1
+dry2: dry1.c dry2.c
+	${CC} -c ${CFLAGS} dry1.c -o dry1.o
+	${CC} ${CFLAGS} dry2.c dry1.o ${LFLAGS} -o dry2 
 
-dry2nr: dry.c
-	${CC}    -c -DREG ${CFLAGS} dry.c -o dry1.o || exit 1
-	${CC} -DPASS2 -DREG ${CFLAGS} dry.c dry1.o ${LFLAGS} -o dry2nr || exit 1
+dry2nr: dry1.c dry2.c
+	${CC} -c -DREG ${CFLAGS} dry1.c -o dry1.o
+	${CC} -DREG ${CFLAGS} dry2.c dry1.o ${LFLAGS} -o dry2nr
 
-dry2o: dry.c
-	${CC}    -c -O ${CFLAGS} dry.c -o dry1.o || exit 1
-	${CC} -DPASS2 -O ${CFLAGS} dry.c dry1.o ${LFLAGS} -o dry2o || exit 1
+dry2o: dry1.c dry2.c
+	${CC} -c -O ${CFLAGS} dry1.c -o dry1.o
+	${CC} -O ${CFLAGS} dry2.c dry1.o ${LFLAGS} -o dry2o
 
 clean:
-	rm -f dry2 dry2nr dry2o
+	rm -f dry1.o dry2 dry2nr dry2o
---
+++ dry.h
@@ -0,0 +1,71 @@
+#define Version "C, Version 2.2"
+
+/* Compiler and system dependent definitions: */
+
+/* variables for time measurement: */
+
+#define CLOCK_TYPE "times()"
+#include <sys/types.h>
+#include <sys/times.h>
+#ifndef HZ	/* Added by SP 900619 */
+#include <sys/param.h> /* If your system doesn't have this, use -DHZ=xxx */
+#else
+	*** You must define HZ!!! ***
+#endif /* HZ */
+#ifndef PASS2
+struct tms      time_info;
+#endif
+/*extern  int     times ();*/
+                /* see library function "times" */
+#define Too_Small_Time (2*HZ)
+                /* Measurements should last at least about 2 seconds */
+#define Start_Timer() times(&time_info); Begin_Time=(long)time_info.tms_utime
+#define Stop_Timer()  times(&time_info); End_Time = (long)time_info.tms_utime
+
+
+#define Mic_secs_Per_Second     1000000.0
+#define NUMBER_OF_RUNS		50000 /* Default number of runs */
+
+#define structassign(d, s)      d = s
+
+  typedef       enum    {Ident_1, Ident_2, Ident_3, Ident_4, Ident_5}
+                Enumeration;
+
+/* General definitions: */
+
+#include <stdio.h>
+                /* for strcpy, strcmp */
+
+#define Null 0 
+                /* Value of a Null pointer */
+#define true  1
+#define false 0
+
+typedef int     One_Thirty;
+typedef int     One_Fifty;
+typedef char    Capital_Letter;
+typedef int     Boolean;
+typedef char    Str_30 [31];
+typedef int     Arr_1_Dim [50];
+typedef int     Arr_2_Dim [50] [50];
+
+typedef struct record 
+    {
+    struct record *Ptr_Comp;
+    Enumeration    Discr;
+    union {
+          struct {
+                  Enumeration Enum_Comp;
+                  int         Int_Comp;
+                  char        Str_Comp [31];
+                  } var_1;
+          struct {
+                  Enumeration E_Comp_2;
+                  char        Str_2_Comp [31];
+                  } var_2;
+          struct {
+                  char        Ch_1_Comp;
+                  char        Ch_2_Comp;
+                  } var_3;
+          } variant;
+      } Rec_Type, *Rec_Pointer;
